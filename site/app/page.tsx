709bf7769f7c2e4cf2946658c9b155f023d47e58
eludda <ee@vech.fun>
2026-04-03 22:10:03 +0800

    eludda created a commit ✨

---
+++ site/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between"></main>
+    </div>
+  )
+}
